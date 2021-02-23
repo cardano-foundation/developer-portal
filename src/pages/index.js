2246301c1cff891505abe030c47a6aed5eebd814
katomm <tk@digitalsirup.com>
2021-02-23 17:59:26 +0100

    Fixed title and description.

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,8 +87,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Overview`}
+      description="Cardano Developer Portal">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
