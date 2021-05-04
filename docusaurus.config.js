6735701c0af3a17aa32556c262f8af641ced2c73
katomm <tk@digitalsirup.com>
2021-05-04 16:35:53 +0200

    Update code styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,26 +1,23 @@
-
-
-// GitHub Settings  
-const repository = 'https://github.com/cardano-foundation/developer-portal';
-const branch     = 'staging';
+// GitHub Settings
+const repository = "https://github.com/cardano-foundation/developer-portal";
+const branch = "staging";
 
 // Docusaurus Config
 module.exports = {
-  title: 'Cardano Developer Portal',
-  tagline: 'together, we can change the world for the better',
-  url: 'https://staging-dev-portal.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'cardano-foundation',
-  projectName: 'developer-portal',
+  title: "Cardano Developer Portal",
+  tagline: "together, we can change the world for the better",
+  url: "https://staging-dev-portal.netlify.app",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "cardano-foundation",
+  projectName: "developer-portal",
   customFields: {
-    repository : `${repository}`,
-    branch     : `${branch}`,
+    repository: `${repository}`,
+    branch: `${branch}`,
   },
   themeConfig: {
-
     // Docs Sidebar
     hideableSidebar: true,
 
@@ -30,154 +27,151 @@ module.exports = {
       // with true defaultMode is overridden by user system preferences.
       respectPrefersColorScheme: true,
       switchConfig: {
-        darkIcon: '🌙',
+        darkIcon: "🌙",
         darkIconStyle: {
-          marginLeft: '2px',
+          marginLeft: "2px",
         },
-        lightIcon: '☀️',
+        lightIcon: "☀️",
         lightIconStyle: {
-          marginLeft: '1px',
+          marginLeft: "1px",
         },
       },
     },
 
     // Announcement Bar
     announcementBar: {
-      id: 'support_us', // Any value that will identify this message.
+      id: "support_us", // Any value that will identify this message.
       content:
-      //`If you like the new portal, give it a star on <a target="_blank" rel="noopener noreferrer" href="${repository}">GitHub</a>! ⭐️`,
-      `<strong>Support the Cardano Stack Exchange proposal <a target="_blank" rel="noopener noreferrer" href="https://area51.stackexchange.com/proposals/125174/cardano?referrer=YWI2YzZhYjZmZTVjODFjOTllMDk5YTU2MDk3YjM1NjE2NTRjZjk1NzUzOGVjNjg5ZWEzNTA5NjQ3YjQyNTg4Ne5A16BO-qeH4pXFAL-53fFirIzQZfSEYKOnpbAIMnZD0">by clicking here</a></strong>`,
-      backgroundColor: '#FD7575', // Defaults to `#fff`.
-      textColor: '#ffffff', // Defaults to `#000`.
+        //`If you like the new portal, give it a star on <a target="_blank" rel="noopener noreferrer" href="${repository}">GitHub</a>! ⭐️`,
+        `<strong>Support the Cardano Stack Exchange proposal <a target="_blank" rel="noopener noreferrer" href="https://area51.stackexchange.com/proposals/125174/cardano?referrer=YWI2YzZhYjZmZTVjODFjOTllMDk5YTU2MDk3YjM1NjE2NTRjZjk1NzUzOGVjNjg5ZWEzNTA5NjQ3YjQyNTg4Ne5A16BO-qeH4pXFAL-53fFirIzQZfSEYKOnpbAIMnZD0">by clicking here</a></strong>`,
+      backgroundColor: "#FD7575", // Defaults to `#fff`.
+      textColor: "#ffffff", // Defaults to `#000`.
       isCloseable: true, // Defaults to `true`.
     },
 
-
     // Meta Image that will be used for your meta tag, in particular og:image and twitter:image
     // Relative to your site's "static" directory, cannot be SVGs.
-    image: 'mg/og-developer-portal.png',
-    metadatas: [{name: 'twitter:card', content: 'summary'}],
-
+    image: "mg/og-developer-portal.png",
+    metadatas: [{ name: "twitter:card", content: "summary" }],
 
     // Algolia Search
     algolia: {
-      appId: '6QH8YVQXAE',
-      apiKey: '6033c09f3af6454c8c25efce0460b84a',
-      indexName: 'developer-portal',
+      appId: "6QH8YVQXAE",
+      apiKey: "6033c09f3af6454c8c25efce0460b84a",
+      indexName: "developer-portal",
       contextualSearch: true,
     },
 
     // Navbar title, logo and items
     navbar: {
       hideOnScroll: false,
-      title: 'Developers',
+      title: "Developers",
       logo: {
-        alt: 'Cardano Logo',
-        src: 'img/cardano-black.svg',
-        srcDark: 'img/cardano-white.svg',
+        alt: "Cardano Logo",
+        src: "img/cardano-black.svg",
+        srcDark: "img/cardano-white.svg",
       },
 
       items: [
         {
-          to: 'tools',
-          label: 'Tools',
-          position: 'left'
+          to: "tools",
+          label: "Tools",
+          position: "left",
         },
         {
-          to: 'showcase',
-          label: 'Showcase',
-          position: 'left'
+          to: "showcase",
+          label: "Showcase",
+          position: "left",
         },
         {
-          to: 'blog/',
-          label: 'Dev Blog',
-          position: 'left'
+          to: "blog/",
+          label: "Dev Blog",
+          position: "left",
         },
         {
-          href: 'https://docs.cardano.org/en/latest/',
-          label: 'Docs',
-          position: 'left',
+          href: "https://docs.cardano.org/en/latest/",
+          label: "Docs",
+          position: "left",
         },
         {
           href: `${repository}`,
-          position: 'right',
-          className: 'header-github-link',
+          position: "right",
+          className: "header-github-link",
         },
-
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Developer Portal',
+          title: "Developer Portal",
           items: [
             {
-              label: 'Contributors',
-              to: 'docs/portal-contributors/',
+              label: "Contributors",
+              to: "docs/portal-contributors/",
             },
             {
-              label: 'How to Contribute',
-              to: 'docs/portal-contribute/',
+              label: "How to Contribute",
+              to: "docs/portal-contribute/",
             },
             {
-              label: 'Style Guide',
-              to: 'docs/portal-style-guide',
-            },
-            
-          ],
-        },
-        {
-          title: 'Developer Community',
-          items: [
-            {
-              label: 'Cardano Forum',
-              href: 'https://forum.cardano.org/c/developers/29',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/kfATXEENPD',
-            },
-            {
-              label: 'Reddit',
-              href: 'https://www.reddit.com/r/CardanoDevelopers/',
-            },
-            {
-              label: 'Stack Exchange',
-              href: 'https://area51.stackexchange.com/proposals/125174/cardano',
-            },
-            {
-              label: 'Slack',
-              href: 'https://join.slack.com/t/iohkdevcommunity/shared_invite/zt-mdvb06fr-8Tv8pjl~iR0~lGrimqK_yg',
-            },
-            {
-              label: 'Telegram',
-              href: 'https://t.me/CardanoDevelopersOfficial',
+              label: "Style Guide",
+              to: "docs/portal-style-guide",
             },
           ],
         },
         {
-          title: 'More about Cardano',
+          title: "Developer Community",
           items: [
             {
-              label: 'Cardano Enterprise',
-              href: 'https://cardano.org/enterprise',
+              label: "Cardano Forum",
+              href: "https://forum.cardano.org/c/developers/29",
             },
             {
-              label: 'Cardano Foundation',
-              href: 'https://www.cardanofoundation.org',
+              label: "Discord",
+              href: "https://discord.gg/kfATXEENPD",
             },
             {
-              label: 'Development Updates',
-              to: 'https://cardanoupdates.com',
+              label: "Reddit",
+              href: "https://www.reddit.com/r/CardanoDevelopers/",
             },
             {
-              label: 'Ouroboros Protocol',
-              to: 'https://cardano.org/ouroboros/',
+              label: "Stack Exchange",
+              href: "https://area51.stackexchange.com/proposals/125174/cardano",
             },
             {
-              label: 'Why Cardano?',
-              href: 'https://why.cardano.org',
+              label: "Slack",
+              href:
+                "https://join.slack.com/t/iohkdevcommunity/shared_invite/zt-mdvb06fr-8Tv8pjl~iR0~lGrimqK_yg",
+            },
+            {
+              label: "Telegram",
+              href: "https://t.me/CardanoDevelopersOfficial",
+            },
+          ],
+        },
+        {
+          title: "More about Cardano",
+          items: [
+            {
+              label: "Cardano Enterprise",
+              href: "https://cardano.org/enterprise",
+            },
+            {
+              label: "Cardano Foundation",
+              href: "https://www.cardanofoundation.org",
+            },
+            {
+              label: "Development Updates",
+              to: "https://cardanoupdates.com",
+            },
+            {
+              label: "Ouroboros Protocol",
+              to: "https://cardano.org/ouroboros/",
+            },
+            {
+              label: "Why Cardano?",
+              href: "https://why.cardano.org",
             },
           ],
         },
@@ -189,7 +183,7 @@ module.exports = {
   },
   plugins: [
     [
-      '@docusaurus/plugin-ideal-image',
+      "@docusaurus/plugin-ideal-image",
       {
         quality: 70,
         max: 1030, // max resized image's size.
@@ -200,22 +194,20 @@ module.exports = {
   ],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-          `${repository}/edit/${branch}`,
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: `${repository}/edit/${branch}`,
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-          `${repository}/edit/${branch}`,
+          editUrl: `${repository}/edit/${branch}`,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- sidebars.js
+++ sidebars.js
@@ -1,117 +1,105 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': [
-      'getting-started/overview',
-      'getting-started/blockfrost',
-      'getting-started/cardano-serialization-lib',
-      'getting-started/cardanocli-js',
-      'getting-started/dandelion-apis'
+    "Getting Started": [
+      "getting-started/overview",
+      "getting-started/blockfrost",
+      "getting-started/cardano-serialization-lib",
+      "getting-started/cardanocli-js",
+      "getting-started/dandelion-apis",
     ],
-    'Transaction Metadata': [
-      'transaction-metadata/overview',
-      'transaction-metadata/how-to-create-a-metadata-transaction',
+    "Transaction Metadata": [
+      "transaction-metadata/overview",
+      "transaction-metadata/how-to-create-a-metadata-transaction",
     ],
-    'Cardano Integration': [
-      'cardano-integration/overview',
+    "Cardano Integration": ["cardano-integration/overview"],
+    "Native Tokens": [
+      "native-tokens/overview",
+      "native-tokens/submit-entry-to-cardano-token-registry",
     ],
-    'Native Tokens': [
-      'native-tokens/overview',
-      'native-tokens/submit-entry-to-cardano-token-registry',
-    ],
-    'Stake Pool Operation': [
-      'stake-pool-operation/overview',
+    "Stake Pool Operation": [
+      "stake-pool-operation/overview",
 
       {
-        type: 'category',
-        label: 'Stake Pool Course',
+        type: "category",
+        label: "Stake Pool Course",
         items: [
-                  'stake-pool-course/overview',
-                  'stake-pool-course/introduction-to-cardano',
-                  'stake-pool-course/lesson-1',
-                  'stake-pool-course/lesson-2',
-                  'stake-pool-course/lesson-3',
-                  'stake-pool-course/lesson-4',
-                  'stake-pool-course/lesson-5',
-                  {
-                    type: 'category',
-                    label: 'Handbook',
-                    items: [
-                              'stake-pool-course/handbook/setup-virtual-box-written',
-                              'stake-pool-course/handbook/setup-a-server-on-aws-written',
-                              'stake-pool-course/handbook/setup-firewall',
-                              'stake-pool-course/handbook/install-cardano-node-written',
-                              'stake-pool-course/handbook/run-cardano-node-handbook',
-                              'stake-pool-course/handbook/use-cli',
-                              'stake-pool-course/handbook/utxo-model',
-                              'stake-pool-course/handbook/keys-addresses',
-                              'stake-pool-course/handbook/create-simple-transaction',
-                              'stake-pool-course/handbook/create-stake-pool-keys',
-                              'stake-pool-course/handbook/register-stake-keys',
-                              'stake-pool-course/handbook/generate-stake-pool-keys',
-                              'stake-pool-course/handbook/configure-topology-files',
-                              'stake-pool-course/handbook/register-stake-pool-metadata',
-                              'stake-pool-course/handbook/apply-logging-prometheus',
-                            ],
-                  },
-                {
-                  type: 'category',
-                  label: 'Assignments',
-                  items: [
-                            'stake-pool-course/assignments/assignment-1',
-                            'stake-pool-course/assignments/assignment-2',
-                            'stake-pool-course/assignments/kes_period',
-                          ],
-                },
-                ],
+          "stake-pool-course/overview",
+          "stake-pool-course/introduction-to-cardano",
+          "stake-pool-course/lesson-1",
+          "stake-pool-course/lesson-2",
+          "stake-pool-course/lesson-3",
+          "stake-pool-course/lesson-4",
+          "stake-pool-course/lesson-5",
+          {
+            type: "category",
+            label: "Handbook",
+            items: [
+              "stake-pool-course/handbook/setup-virtual-box-written",
+              "stake-pool-course/handbook/setup-a-server-on-aws-written",
+              "stake-pool-course/handbook/setup-firewall",
+              "stake-pool-course/handbook/install-cardano-node-written",
+              "stake-pool-course/handbook/run-cardano-node-handbook",
+              "stake-pool-course/handbook/use-cli",
+              "stake-pool-course/handbook/utxo-model",
+              "stake-pool-course/handbook/keys-addresses",
+              "stake-pool-course/handbook/create-simple-transaction",
+              "stake-pool-course/handbook/create-stake-pool-keys",
+              "stake-pool-course/handbook/register-stake-keys",
+              "stake-pool-course/handbook/generate-stake-pool-keys",
+              "stake-pool-course/handbook/configure-topology-files",
+              "stake-pool-course/handbook/register-stake-pool-metadata",
+              "stake-pool-course/handbook/apply-logging-prometheus",
+            ],
+          },
+          {
+            type: "category",
+            label: "Assignments",
+            items: [
+              "stake-pool-course/assignments/assignment-1",
+              "stake-pool-course/assignments/assignment-2",
+              "stake-pool-course/assignments/kes_period",
+            ],
+          },
+        ],
       },
-  ],
-    'Project Funding': [
-      'funding/overview',
-      'funding/dcfund',
-      'funding/cfund',
     ],
-    'Contribution': [
-        'portal-contributors',
-        'portal-contribute',
-        'portal-style-guide',
+    "Project Funding": ["funding/overview", "funding/dcfund", "funding/cfund"],
+    Contribution: [
+      "portal-contributors",
+      "portal-contribute",
+      "portal-style-guide",
     ],
-    'Unused': [
+    Unused: [
       {
-        type: 'category',
-        label: 'Plutus',
+        type: "category",
+        label: "Plutus",
         items: [
-                  'unused/smart-contracts-and-building-dapps/plutus/plutus-overview',
-                ],
+          "unused/smart-contracts-and-building-dapps/plutus/plutus-overview",
+        ],
       },
       {
-        type: 'category',
-        label: 'Community',
-        items: [
-                  'unused/community-ambassador-program',
-                ],
+        type: "category",
+        label: "Community",
+        items: ["unused/community-ambassador-program"],
       },
       {
-        type: 'category',
-        label: 'Resources',
-        items: [
-                  'unused/resources/developer-portal-updates',
-                ],
+        type: "category",
+        label: "Resources",
+        items: ["unused/resources/developer-portal-updates"],
       },
       {
-        type: 'category',
-        label: 'Adrestia - SDKs and APIs',
-        items: [
-                  'unused/adrestia-SDKs-and-APIs/adrestia-cardano-node',
-                ],
+        type: "category",
+        label: "Adrestia - SDKs and APIs",
+        items: ["unused/adrestia-SDKs-and-APIs/adrestia-cardano-node"],
       },
       {
-        type: 'category',
-        label: 'Learn Cardano',
+        type: "category",
+        label: "Learn Cardano",
         items: [
-                  'unused/learn-cardano/cardano-node',
-                  'unused/learn-cardano/token-locking',
-                ],
+          "unused/learn-cardano/cardano-node",
+          "unused/learn-cardano/token-locking",
+        ],
       },
-  ],
+    ],
   },
 };
