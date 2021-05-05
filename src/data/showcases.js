6df1fc158982b924767769eeccf87a0c2364293f
katomm <tk@digitalsirup.com>
2021-05-05 09:36:53 +0200

    Add category/tag feature in showcase, sorting, value checking, check if open source tags have the open source field

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1,36 +1,55 @@
 /*
- * ADD YOUR SITE TO THE CARDANO DEVELOPER PORTAL SHOWCASE:
+ * ADD YOUR PROJECT TO THE CARDANO DEVELOPER PORTAL SHOWCASE:
  *
- * Requirements for adding your site to our showcase:
- * - It must be built on Cardano and have a real usecase. Preferably but not necessarily dapps.
- *   Please exclude pooltools and explorers for the time being.
+ * Requirements for adding your project to our showcase:
+ * - It must be built on Cardano and have a real usecase. 
  * - It has a stable domain name (a random Netlify/Vercel domain is not allowed)
- * - The code is publicly available (not decided yet if this is a requirement)
  *
  * Instructions:
- * - Add your site in the json array below, in alphabetical order of title
- * - Add a local image preview (decent screenshot of your site)
+ * - Add your project in the json array below
+ * - Add a local image preview (decent screenshot of your project)
  *
  * The image must be added to the GitHub repository, and use `require("image")`
- *
- * Example PR: FIXME: provide example PR in the future
- *
- * If you edit this file through the Github interface, you can:
- * - Submit first your showcase.js edit PR
- * - This will create a branch on your Docusaurus fork (usually "patch-1")
- * - Go to https://github.com/<username>/developer-portal/tree/<branch>/website/src/data/showcase
- * - Drag-and-drop an image here to add it to your existing PR
- *
  */
 
-const showcases = [
+import React from "react";
+import { sortBy, difference } from "../utils/jsUtils";
+
+// List of available tags. (PLEASE DO NOT ADD NEW TAGS)
+export const Tags = {
+  // PLEASE DO NOT USE THIS TAG: we choose the features projects (process TBD)
+  featured: {
+    label: "Featured",
+    description:
+      "Our favorite Cardano projects that you must absolutely check-out.",
+    icon: <>⭐️</>,
+  },
+
+  // For open-source sites, a link to the source code is required
+  opensource: {
+    label: "Open-Source",
+    description: "Open-Source sites can be useful for inspiration.",
+    icon: null,
+  },
+
+  // Native tokens related projects, maybe later we distinguish between NFT
+  tokens: {
+    label: "Native Tokens",
+    description: "Native Tokens",
+    icon: null,
+  },
+};
+
+// Add your site to this list
+const Showcases = [
   {
     title: "Cardano Kidz",
     description:
       "Cardano non fungible token (NFT) design cards in lovely design.",
     preview: require("./showcase/cardanokidz.png"),
     website: "https://www.cardanokidz.com",
-    // offers no source
+    source: null,
+    tags: ["tokens"],
   },
   {
     title: "Cardano Wall",
@@ -38,7 +57,8 @@ const showcases = [
       "Demonstrates serveral use cases for transaction metdata. You can sign messages and create proof of existence for files.",
     preview: require("./showcase/cardanowall.png"),
     website: "https://cardanowall.com/en/explore/",
-    // offers no source
+    source: null,
+    tags: ["featured"],
   },
   {
     title: "Meta Data Connector",
@@ -46,7 +66,8 @@ const showcases = [
       "Cardano Foundation’s supply chain traceability and anti-counterfeit solution together with Scantrust.",
     preview: require("./showcase/baiawine.png"),
     website: "https://www.baiawine.com",
-    // source: 'https://github.com/cardano-foundation/FIXME', // will offer source
+    source: "https://github.com/cardano-foundation",
+    tags: ["featured", "opensource"],
   },
   {
     title: "NFT Maker",
@@ -54,7 +75,8 @@ const showcases = [
       "Create your own NFT by uploading an image and paying some ada.",
     preview: require("./showcase/nft-maker.png"),
     website: "https://www.nft-maker.io",
-    // offers no source
+    source: null,
+    tags: ["tokens"],
   },
   {
     title: "SpaceBudz",
@@ -62,21 +84,132 @@ const showcases = [
       "The first full on-chain NFT platform on Cardano consisting of 10,000 unique little astronauts.",
     preview: require("./showcase/spacebudz.png"),
     website: "https://spacebudz.io",
-    // offers no source
+    source: null,
+    tags: ["tokens"],
   },
 ];
 
-showcases.forEach((showcase) => {
-  if (
-    !showcase.preview ||
-    (showcase.preview instanceof String &&
-      (showcase.preview.startsWith("http") ||
-        showcase.preview.startsWith("//")))
-  ) {
+export const TagList = Object.keys(Tags);
+function sortShowcases() {
+  let result = Showcases;
+  // Sort by site name
+  result = sortBy(result, (showcase) => showcase.title.toLowerCase());
+  // Sort by featured tag, featured first
+  result = sortBy(result, (showcase) => !showcase.tags.includes("featured"));
+  return result;
+}
+
+export const SortedShowcases = sortShowcases();
+
+// Fail-fast on common errors
+function ensureShowcaseValid(showcase) {
+  function checkFields() {
+    const keys = Object.keys(showcase);
+    const validKeys = [
+      "title",
+      "description",
+      "preview",
+      "website",
+      "source",
+      "tags",
+    ];
+    const unknownKeys = difference(keys, validKeys);
+    if (unknownKeys.length > 0) {
+      throw new Error(
+        `Site contains unknown attribute names=[${unknownKeys.join(",")}]`
+      );
+    }
+  }
+
+  function checkTitle() {
+    if (!showcase.title) {
+      throw new Error("Site title is missing");
+    }
+  }
+
+  function checkDescription() {
+    if (!showcase.description) {
+      throw new Error("Site description is missing");
+    }
+  }
+
+  function checkWebsite() {
+    if (!showcase.website) {
+      throw new Error("Site website is missing");
+    }
+    const isHttpUrl =
+      showcase.website.startsWith("http://") ||
+      showcase.website.startsWith("https://");
+    if (!isHttpUrl) {
+      throw new Error(
+        `Site website does not look like a valid url: ${showcase.website}`
+      );
+    }
+  }
+
+  function checkPreview() {
+    if (
+      !showcase.preview ||
+      (showcase.preview instanceof String &&
+        (showcase.preview.startsWith("http") ||
+          showcase.preview.startsWith("//")))
+    ) {
+      throw new Error(
+        `Site has bad image preview=[${showcase.preview}].\nThe image should be hosted on the Developer Portal GitHub, and not use remote HTTP or HTTPS URLs`
+      );
+    }
+  }
+
+  function checkTags() {
+    if (
+      !showcase.tags ||
+      !(showcase.tags instanceof Array) ||
+      showcase.tags.includes("")
+    ) {
+      throw new Error(`Bad showcase tags=[${JSON.stringify(showcase.tags)}]`);
+    }
+    const unknownTags = difference(showcase.tags, TagList);
+    if (unknownTags.length > 0) {
+      throw new Error(
+        `Unknown tags=[${unknownTags.join(
+          ","
+        )}\nThe available tags are ${TagList.join(",")}`
+      );
+    }
+  }
+
+  function checkOpenSource() {
+    if (typeof showcase.source === "undefined") {
+      throw new Error(
+        "The source attribute is required.\nIf your Cardano project is not open-source, please make it explicit with 'source: null'"
+      );
+    } else {
+      const hasOpenSourceTag = showcase.tags.includes("opensource");
+      if (showcase.source === null && hasOpenSourceTag) {
+        throw new Error(
+          "You can't add the opensource tag to a site that does not have a link to source code."
+        );
+      } else if (showcase.source && !hasOpenSourceTag) {
+        throw new Error(
+          "For open-source sites, please add the 'opensource' tag."
+        );
+      }
+    }
+  }
+
+  try {
+    checkFields();
+    checkTitle();
+    checkDescription();
+    checkWebsite();
+    checkPreview();
+    checkTags();
+    checkOpenSource();
+  } catch (e) {
     throw new Error(
-      `Bad showcase site image preview = ${showcase.preview}. The image should be hosted on developer portal site, and not use remote HTTP or HTTPS URLs`
+      `Showcase site with title=${showcase.title} contains errors:\n${e.message}`
     );
   }
-});
+}
 
-export default showcases;
+Showcases.forEach(ensureShowcaseValid);
