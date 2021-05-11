f4f7bb513c5ce893e5fa05c1b1fbd7cd92af743c
Clark Alesna <clark_alesna@hotmail.com>
2021-05-11 21:22:03 +0800

    Initial Listening for ADA Payments documentation

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,10 @@ module.exports = {
       "transaction-metadata/overview",
       "transaction-metadata/how-to-create-a-metadata-transaction",
     ],
-    "Cardano Integration": ["cardano-integration/overview"],
+    "Cardano Integration": [
+      "cardano-integration/overview",
+      "cardano-integration/listening-for-payments"
+    ],
     "Native Tokens": [
       "native-tokens/overview",
       "native-tokens/submit-entry-to-cardano-token-registry",
