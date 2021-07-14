7f44c6a46622927d44608b312f195b4c836acc06
katomm <tk@digitalsirup.com>
2021-07-14 11:31:56 +0200

    Fix dot

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -5,7 +5,7 @@
  * - It must be built on Cardano and have a real use case. For example, a forum where 
  *   people can talk about Cardano is great, but nothing for this showcase section.
  * - It needs to run on Cardano mainnet.
- * - It needs to have enough community reputation
+ * - It needs to have enough community reputation.
  * - It needs to provide a unique value from existing showcase items. (we can't list 
  *   thousands of NFT or native tokens with the current UI)
  * - It has a stable domain name. (a random Netlify/Vercel domain is not allowed, no 
