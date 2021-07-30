09bf36290f2b5e13d3235c5cdc564c77dbf29156
Tommy <31965230+katomm@users.noreply.github.com>
2021-07-30 15:35:13 +0200

    Replace announcement with Cardano Stack Exchange (#200)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,9 +46,9 @@ module.exports = {
     // id: always change it when changing the announcement
     // backgroundColor: use #FD7575 for warnings and #2AA18A for announcements
     announcementBar: {
-      id: "survey_op1", // Any value that will identify this message.
+      id: "support_se2", // Any value that will identify this message + increment the number every time to be unique
       content:
-        `<strong>Welcome to the new Developer Portal.🎉 Your opinion matters! Please take 2 minutes to <a target="_blank" rel="noopener noreferrer" href="https://cardanocommunity.typeform.com/to/f2lKIRB6">fill out our survey</a>.</strong>`,
+        `<strong>Join the Cardano Stack Exchange and get your questions answered today. ⭐️<a target="_blank" rel="noopener noreferrer" href="https://cardano.stackexchange.com">Join us!</a></strong>`,
       backgroundColor: "#2AA18A", 
       textColor: "#FFFFFF", // Use #FFFFFF
       isCloseable: true, // Use true
