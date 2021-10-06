6e8bddac015f220c3ce7c57f3cf2075b9708404d
Leobel Izquierdo <leobelizquierdo@gmail.com>
2021-10-06 05:11:01 -0300

    Add cardano-wallet-js (#313)
    * cardano-wallet-js
    
    * fix description
    
    * fix comments
    
    * include tags and fix typo
    
    * adapt get started page to styling guide
    
    Co-authored-by: javiertc

--- sidebars.js
+++ sidebars.js
@@ -18,6 +18,7 @@ module.exports = {
         items: [
           "get-started/blockfrost",
           "get-started/cardanocli-js",
+          "get-started/cardano-wallet-js",
           "get-started/dandelion-apis",
           "get-started/ogmios",
           "get-started/cardanosharp-wallet",
