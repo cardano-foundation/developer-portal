3daf51d6c8e5d3a992861bf5338beb04592238ed
PS Max Zaxster <89811300+PSMaxZaxster@users.noreply.github.com>
2022-02-24 14:45:33 +0100

    Update showcases.js

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -943,7 +943,7 @@ Showcases.forEach(ensureShowcaseValid);
     title: "PS Max Zaxster",
     description:
       "We are painting studio and transforming our painters work into NFT. Finishing our special crypto collection, cards of famous painters in 2k collection.",
-    preview: require("./showcase/maxzaxster.png"),
+    preview: require("./showcase/psmaxzaxster.png"),
     website: "https://www.maxzaxster.com/",
     source: null,
     tags: ["NFT project"],
