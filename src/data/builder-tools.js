a72c2ea3ab22acaeb1d2057deb5962c931089068
Harmonic <96301444+HarmonicPool@users.noreply.github.com>
2022-03-03 12:22:11 +0100

    add getStarde page in cardano-wallet-interface

--- src/data/builder-tools.js
+++ src/data/builder-tools.js
@@ -327,8 +327,8 @@ const Showcases = [
     description: "A Javascript/Typescript library to easily interact with the dApp connector of various wallets.",
     preview: require("./builder-tools/cardano-wallet-interface.png"),
     website: "https://github.com/HarmonicPool/cardano-wallet-interface",
-    getstarted: null,
-    tags: ["library", "javascript"],
+    getstarted: "https://github.com/HarmonicPool/cardano-wallet-interface/tree/main/documentation,
+    tags: ["getstarted", "library", "javascript"],
   },
   {
     title: "Plutus Fee Estimator",
