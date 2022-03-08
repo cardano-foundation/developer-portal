e5489162c378c48ea065f6753a894eee6c1f3590
iammisai <99393784+iammisai@users.noreply.github.com>
2022-03-08 20:50:50 +0800

    Update showcases.js

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -824,7 +824,7 @@ const Showcases = [
       "HoskySwap is a Cardano feature that allows $ADA holders to buy, sell or swap their $Hosky tokens and connect directly to their wallets.",
     preview: require("./showcase/hoskyswap.png"),
     website: "https://hosky-swap.io/",
-    source: null,
+    source: "https://github.com/ADAPhilippines/HOSKYSWAP",
     tags: ["opensource", "marketplace"],
   },
 ];
