6073b39d6acb79e5449dc49208cb1fed9af37015
Fillips Ickevics <60065019+fill-the-fill@users.noreply.github.com>
2022-04-05 13:20:39 +0300

    Add showcase project Atala Scan (#575)
    * Add showcase project Atala Scan
    
    * Change tags for Atala Scan
    
    * Add identity tag and modify AtalaScan image
    
    Co-authored-by: fill-the-fill <fill.ickevics@gmail.com>
    Co-authored-by: Tommy Kammerer <31965230+katomm@users.noreply.github.com>

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -163,6 +163,14 @@ export const Tags = {
     icon: null,
     color: '#1B32F0'
   },
+
+  // Identity
+  identity: {
+    label: "Identity",
+    description: "Decentralized identifiers (DIDs)",
+    icon: null,
+    color: '#000'
+  },
 };
 
 // Add your project to (THE END OF) this list.
@@ -843,6 +851,15 @@ const Showcases = [
     website: "https://app.minswap.org",
     source: null,
     tags: [ "dex", "token"],
+  },
+  {
+    title: "Atala Scan",
+    description:
+      "Easy-to-use mobile app that gives customers real-time proof of products’ authenticity, recorded on the Cardano blockchain for life.",
+    preview: require("./showcase/atalascan.png"),
+    website: "https://atalascan.io",
+    source: null,
+    tags: ["metadata", "identity"],
   },
   {
     title: "Pavia",
