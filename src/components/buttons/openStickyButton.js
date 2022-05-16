40142bf96e741e97abab3639c32dc9621bef8bb4
Fillips Ickevics <60065019+fill-the-fill@users.noreply.github.com>
2022-05-16 16:42:32 +0300

    Fix sticky button to be clickable anywhere (#618)
    * Fix sticky button to be clickable anywhere
    
    * Remove target blank from sticky button
    
    * Pretitfy code in sticky button
    
    Co-authored-by: fill-the-fill <fill.ickevics@gmail.com>

--- src/components/buttons/openStickyButton.js
+++ src/components/buttons/openStickyButton.js
@@ -3,17 +3,13 @@ import styles from "./styles.module.css";
 
 export default function openStickyButton() {
   return (
-    <div>
+    <a href="https://developers.cardano.org/docs/portal-contribute/">
       <button className={`${styles.iconBtn} ${styles.addBtn}`}>
         <div className={styles.addIcon}></div>
-        <a
-          className={styles.btnText}
-          href="https://developers.cardano.org/docs/portal-contribute/"
-          target="_blank"
-        >
+        <div className={styles.btnText}>
           <span className={styles.btnSpan}>CONTRIBUTE NOW</span>
-        </a>
+        </div>
       </button>
-    </div>
+    </a>
   );
 }
