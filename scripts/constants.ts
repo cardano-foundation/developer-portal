424501f828ef441bc645268e495f66b7cbb759fb
fill-the-fill <fill.ickevics@gmail.com>
2022-06-02 11:48:31 +0300

    Modify exporting of constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,23 +1,25 @@
 //CIP constants 
-export const CIPReadmeUrl: string = '/README.md';
-export const CIPPReadmeRegex: RegExp = /\.\/CIP.*?\//gm;
-export const CIPSourceRepo: string = 'cardano-foundation/CIPs';
-export const CIPStaticResourcePath: string = '/static/img/cip/';
-export const CIPDocsPath: string = './docs/governance/cardano-improvement-proposals';
-export const CIPRegex: RegExp = /\]\(.*?.png\)|\]\(.*?.jpg\)|\]\(.*?.jpeg\)|\]\(.*?.json\)/gm;
-export const CIPRepoBaseUrl: string = 'https://github.com/cardano-foundation/CIPs/tree/master/';
-export const CIPRepoRawBaseUrl: string = 'https://raw.githubusercontent.com/cardano-foundation/CIPs/master/';
+const CIPReadmeUrl: string = '/README.md';
+const CIPPReadmeRegex: RegExp = /\.\/CIP.*?\//gm;
+const CIPSourceRepo: string = 'cardano-foundation/CIPs';
+const CIPStaticResourcePath: string = '/static/img/cip/';
+const CIPDocsPath: string = './docs/governance/cardano-improvement-proposals';
+const CIPRegex: RegExp = /\]\(.*?.png\)|\]\(.*?.jpg\)|\]\(.*?.jpeg\)|\]\(.*?.json\)/gm;
+const CIPRepoBaseUrl: string = 'https://github.com/cardano-foundation/CIPs/tree/master/';
+const CIPRepoRawBaseUrl: string = 'https://raw.githubusercontent.com/cardano-foundation/CIPs/master/';
 
 // Rust Library constants 
-export const RLStaticResourcePath: string = '/tree/master/doc/getting-started'
-export const RLDocsPath: string = './docs/get-started/cardano-serialization-lib';
-export const RLRepoBaseUrl: string = 'https://github.com/Emurgo/cardano-serialization-lib'
-export const RLnamesRawBaseIndexUrl: string = 'https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/index.rst';
-export const RLRepoRawBaseUrl: string = 'https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/getting-started/';
+const RLStaticResourcePath: string = '/tree/master/doc/getting-started';
+const RLDocsPath: string = './docs/get-started/cardano-serialization-lib';
+const RLRepoBaseUrl: string = 'https://github.com/Emurgo/cardano-serialization-lib';
+const RLnamesRawBaseIndexUrl: string = 'https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/index.rst';
+const RLRepoRawBaseUrl: string = 'https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/getting-started/';
 
 //Token Registry contants
-export const TRDocsPath: string = './docs/native-tokens/token-registry';
-export const TRWiki: string = 'https://github.com/cardano-foundation/cardano-token-registry/wiki';
-export const TRUrl: string = 'https://github.com/cardano-foundation/cardano-token-registry/blob/master/';
-export const TRrepoRawWikiHomeUrl: string = 'https://raw.githubusercontent.com/wiki/cardano-foundation/cardano-token-registry/';
-export const TROverviewUrl: string = 'https://raw.githubusercontent.com/cardano-foundation/cardano-token-registry/master/README.md';
+const TRDocsPath: string = './docs/native-tokens/token-registry';
+const TRWiki: string = 'https://github.com/cardano-foundation/cardano-token-registry/wiki';
+const TRUrl: string = 'https://github.com/cardano-foundation/cardano-token-registry/blob/master/';
+const TRrepoRawWikiHomeUrl: string = 'https://raw.githubusercontent.com/wiki/cardano-foundation/cardano-token-registry/';
+const TROverviewUrl: string = 'https://raw.githubusercontent.com/cardano-foundation/cardano-token-registry/master/README.md';
+
+export  {CIPReadmeUrl, CIPPReadmeRegex, CIPSourceRepo, CIPStaticResourcePath, CIPDocsPath, CIPRegex, CIPRepoBaseUrl, CIPRepoRawBaseUrl, RLStaticResourcePath, RLDocsPath, RLRepoBaseUrl, RLnamesRawBaseIndexUrl, RLRepoRawBaseUrl, TRDocsPath, TRWiki, TRUrl, TRrepoRawWikiHomeUrl, TROverviewUrl};
