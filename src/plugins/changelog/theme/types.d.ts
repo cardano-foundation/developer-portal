fbffd60376dd187c043505b3ce05f895f07902ed
fill-the-fill <fill.ickevics@gmail.com>
2022-09-15 12:01:21 +0700

    Cleanup in plugin

--- src/plugins/changelog/theme/types.d.ts
+++ src/plugins/changelog/theme/types.d.ts
@@ -1,17 +1,8 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 declare module '@theme/ChangelogPaginator';
-
 declare module '@theme/ChangelogItem';
 declare module '@theme/ChangelogItem/Header';
 declare module '@theme/ChangelogItem/Header/Author';
 declare module '@theme/ChangelogItem/Header/Authors';
-
 declare module '@theme/ChangelogList';
 
 declare module '@theme/Icon/Expand' {
