4611af8e27c936c9acb06a014a45259a13edb25a
NithB <102213094+nithb@users.noreply.github.com>
2022-09-18 10:10:38 +0530

    Add Great Empires

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1094,6 +1094,15 @@ export const Showcases = [
     source: null,
     tags: [ "daotool"],
   },
+  {
+    title: "Great Empires",
+    description:
+      "Building ancient era metaverse on Cardano Blockchain. Phase 01 : Digital Collectible Card Game, Phase 02 : Metaverse, Utility token of the ecosystem GREMP",
+    preview: require("./showcase/great_empires.png"),
+    website: "https://greatempires.io/",
+    source: null,
+    tags: [ "game", "nft", "nativetoken"],
+  }
 ];
 
 export const TagList = Object.keys(Tags);
