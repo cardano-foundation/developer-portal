5823e11b9eb6dac07741b6dfcf7c671e087e8a7f
Hong Jing (Jingles) <xiao_laoshu@hotmail.com>
2022-10-05 16:58:28 +0800

    Add Mesh into builder tools and showcases (#784)
    * add Mesh into builder tools and showcases
    
    * update Mesh showcase tags
    
    * Update src/data/builder-tools.js
    
    Co-authored-by: Fill <60065019+fill-the-fill@users.noreply.github.com>
    
    * update Mesh image and tags
    
    * remove Mesh from showcases
    
    * Delete yarn.lock
    
    * fixed my typo in resolving merge conflict
    
    Co-authored-by: Fill <60065019+fill-the-fill@users.noreply.github.com>
    Co-authored-by: Robert Phair <rphair@cosd.com>

--- src/data/builder-tools.js
+++ src/data/builder-tools.js
@@ -763,13 +763,21 @@ export const Showcases = [
     tags: ["operatortool", "cli"],
   },
   {
+    title: "Mesh",
+    description: "An Open-Source library by Martify Labs with the purpose of making building dApps on Cardano easier than ever. The Mesh library is engineered to simplify the development and implementation of blockchain technology, so you can focus on your project.",
+    preview: require("./builder-tools/mesh.png"),
+    website: "https://mesh.martify.io/",
+    getstarted: null,
+    tags: ["javascript", "typescript", "serialization", "sdk", "plutus"]
+  },
+  {
     title: "Cexplorer",
     description: "Rich featured blockchain explorer with support of all testnet instances and an API.",
     preview: require("./builder-tools/cexplorer.png"),
     website: "https://cexplorer.io",
     getstarted: null,
     tags: ["operatortool"],
-  }
+  },
 ];
 
 export const TagList = Object.keys(Tags);
