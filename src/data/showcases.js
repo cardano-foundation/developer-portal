8c5b8c8d5af47799d4c67e584dea6dc551edd1f3
Dario Bugmann <dario.bugmann@outlook.com>
2022-12-31 15:15:10 +0100

    add cardano studio showcase

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1163,6 +1163,15 @@ export const Showcases = [
     website: "https://www.petregistry.io",
     source: null,
     tags: ["metadata", "identity"],
+  }, 
+  {
+    title: "Cardano Studio",
+    description:
+      "Create and mint NFTs on Cardano all in your browser - without trusting a third party for minting.",
+    preview: require("./showcase/cardano-studio.png"),
+    website: "https://cardano-studio.app",
+    source: null,
+    tags: ["minting", "nftsupport"],
   },
 ];
 
