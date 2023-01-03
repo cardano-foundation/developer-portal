4f15dfaec7d379cefeb2bbbea6f2fc0e37929bd7
Robert Phair <rphair@cosd.com>
2023-01-03 17:21:23 +0530

    Add Milkomeda to Showcase (#899)
    * added Milkomeda to showcase + unnecessarily changed yarn.lock
    
    * forgot project featured image
    
    * remove yarn install state for consistency with #900
    
    Co-authored-by: Fill <60065019+fill-the-fill@users.noreply.github.com>

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -510,6 +510,14 @@ export const Showcases = [
     tags: ["favorite", "wallet", "nftsupport"],
   },
   {
+    title: "Milkomeda",
+    description: "Cross-blockchain Level 2 solutions including EVM support on Cardano and wrapped native assets.",
+    preview: require("./showcase/milkomeda.png"),
+    website: "https://milkomeda.com",
+    source: null,
+    tags: ["bridge"]
+  },
+  {
     title: "Jetchicken",
     description:
       "From the guys who brought you spacecoins, comes jetchickens. A collectible trading card game on the Cardano.",
