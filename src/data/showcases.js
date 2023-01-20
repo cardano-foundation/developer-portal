17f6e1b313f0f3c103e38aea815077ee3682fb31
fill-the-fill <fill.ickevics@gmail.com>
2023-01-20 13:48:51 +0200

    Add showcase Relay Map

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1181,6 +1181,15 @@ export const Showcases = [
     source: null,
     tags: ["explorer"],
   },
+  {
+    title: "Cardano Relay Map",
+    description: 
+      "Stake Pool relay nodes worldwide map.",
+    preview: require("./showcase/monadpool-relay-map.png"),
+    website: "https://monadpool.com/cardano.html",
+    source: null,
+    tags: ["analytics"],
+  },
 ];
 
 export const TagList = Object.keys(Tags);
