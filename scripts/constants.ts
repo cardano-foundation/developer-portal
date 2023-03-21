16ee5676d2990a856df1f7cccf2762798b0c01f5
rphair <rphair@cosd.com>
2023-03-21 23:52:21 +0530

    grabbed latest cip.ts + constants.ts from staging

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,6 @@
 //CIP constants 
 const CIPReadmeUrl: string = "/README.md";
-const CIPPReadmeRegex: RegExp = /\.\/CIP.*?\//gm;
+const CIPPReadmeRegex: RegExp = /\.\/CIP.?\/|\.\/CIP-.*\)/gm
 const CIPSourceRepo: string = "cardano-foundation/CIPs";
 const CIPStaticResourcePath: string = "/static/img/cip/";
 const CIPDocsPath: string = "./docs/governance/cardano-improvement-proposals";
@@ -15,11 +15,14 @@ const RLRepoBaseUrl: string = "https://github.com/Emurgo/cardano-serialization-l
 const RLnamesRawBaseIndexUrl: string = "https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/index.rst";
 const RLRepoRawBaseUrl: string = "https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/getting-started/";
 
-//Token Registry contants
+//Token Registry constants
 const TRDocsPath: string = "./docs/native-tokens/token-registry";
 const TRWiki: string = "https://github.com/cardano-foundation/cardano-token-registry/wiki";
 const TRUrl: string = "https://github.com/cardano-foundation/cardano-token-registry/blob/master/";
 const TRrepoRawWikiHomeUrl: string = "https://raw.githubusercontent.com/wiki/cardano-foundation/cardano-token-registry/";
 const TROverviewUrl: string = "https://raw.githubusercontent.com/cardano-foundation/cardano-token-registry/master/README.md";
 
-export  {CIPReadmeUrl, CIPPReadmeRegex, CIPSourceRepo, CIPStaticResourcePath, CIPDocsPath, CIPRegex, CIPRepoBaseUrl, CIPRepoRawBaseUrl, RLStaticResourcePath, RLDocsPath, RLRepoBaseUrl, RLnamesRawBaseIndexUrl, RLRepoRawBaseUrl, TRDocsPath, TRWiki, TRUrl, TRrepoRawWikiHomeUrl, TROverviewUrl};
+//General constants
+const customEditUrl = "\ncustom_edit_url: null"
+
+export  {CIPReadmeUrl, CIPPReadmeRegex, CIPSourceRepo, CIPStaticResourcePath, CIPDocsPath, CIPRegex, CIPRepoBaseUrl, CIPRepoRawBaseUrl, RLStaticResourcePath, RLDocsPath, RLRepoBaseUrl, RLnamesRawBaseIndexUrl, RLRepoRawBaseUrl, TRDocsPath, TRWiki, TRUrl, TRrepoRawWikiHomeUrl, TROverviewUrl, customEditUrl};
