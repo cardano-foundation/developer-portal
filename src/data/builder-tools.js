3b27964cce1fad8389121cbccaeb80f5aa36b4ed
Fill <60065019+fill-the-fill@users.noreply.github.com>
2023-07-06 11:54:25 +0300

    Add Atlas builder tool (#1041)
    Co-authored-by: fill-the-fill <fill.ickevics@gmail.com>
    Co-authored-by: Robert Phair <rphair@cosd.com>

--- src/data/builder-tools.js
+++ src/data/builder-tools.js
@@ -1058,6 +1058,15 @@ export const Showcases = [
     getstarted: null,
     tags: ["nft", "http", "hosted" ]
   },
+  {
+    title: "Atlas",
+    description: 
+      "Atlas is an all-in-one, Haskell-native application backend for writing off-chain code for on-chain Plutus smart contracts.",
+    preview: require("./builder-tools/atlas.png"),
+    website: "https://atlas-app.io/",
+    getstarted: null,
+    tags: ["haskell", "plutus"],
+  },
 ];
 
 export const TagList = Object.keys(Tags);
