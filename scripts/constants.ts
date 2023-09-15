4570c0a530a6b3a980934d457f91855c2f64bdc5
Fill <60065019+fill-the-fill@users.noreply.github.com>
2023-09-15 11:03:18 +0200

    Adjust comment spacing (#1145)
    Co-authored-by: fill-the-fill <fill.ickevics@gmail.com>

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,4 +1,4 @@
-//CIP constants 
+// CIP constants 
 const cip_readme_url: string = "/README.md";
 const cip_readme_regex: RegExp = /\.\/CIP.?\/|\.\/CIP-.*\)/gm
 const cip_source_repo: string = "cardano-foundation/CIPs";
@@ -15,14 +15,14 @@ const rl_repo_base_url: string = "https://github.com/Emurgo/cardano-serializatio
 const rl_raw_base_index_url: string = "https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/index.rst";
 const rl_repo_raw_base_url: string = "https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/getting-started/";
 
-//Token Registry constants
+// Token Registry constants
 const tr_docs_path: string = "./docs/native-tokens/token-registry";
 const tr_github_wiki: string = "https://github.com/cardano-foundation/cardano-token-registry/wiki";
 const tr_url: string = "https://github.com/cardano-foundation/cardano-token-registry/blob/master/";
 const tr_raw_wiki_url: string = "https://raw.githubusercontent.com/wiki/cardano-foundation/cardano-token-registry/";
 const tr_overview_url: string = "https://raw.githubusercontent.com/cardano-foundation/cardano-token-registry/master/README.md";
 
-//General constants
+// General constants
 const custom_edit_url = "\ncustom_edit_url: null"
 
 export  {cip_readme_url, cip_readme_regex, cip_source_repo, cip_static_resource_path, cip_docs_path, cip_regex, cip_repo_base_url, cip_repo_raw_base_url, rl_static_resource_path, rl_docs_path, rl_repo_base_url, rl_raw_base_index_url, rl_repo_raw_base_url, tr_docs_path, tr_github_wiki, tr_url, tr_raw_wiki_url, tr_overview_url, custom_edit_url};
