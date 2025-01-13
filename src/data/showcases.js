554df1da2eeb2703790af670ab49d7e5fc472a33
Robert Phair <rphair@cosd.com>
2025-01-13 14:31:08 +0530

    removing `source:` entry after `opensource` tag removed

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1217,7 +1217,6 @@ export const Showcases = [
       "Begin Wallet a non-custodial light Cardano Wallet, available as an Extension and Mobile. We offer payment link compatibility with deep link support, Begin ID user name based on ENS protocol for Wallet Address. Hardware wallet support Ledger and Keystone. Based on our own open source cryptographic core.",
     preview: require("./showcase/begin.png"),
     website: "https://begin.is",
-    source: "https://github.com/BeginWallet/begin-core",
     tags: ["wallet", "nftsupport"],
   },
 ];
