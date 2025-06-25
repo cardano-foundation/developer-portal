f39fe106d8d122b960ea42eea3b8dbcb5574b1e8
Tucker Triggs <16181700+tuckpuck@users.noreply.github.com>
2025-06-25 10:56:13 +0200

    Add adastack.io to showcase (#1456)
    * Showcase: add Adastack.io project with preview image
    
    * quick spell correction
    
    ---------
    
    Co-authored-by: Robert Phair <rphair@cosd.com>

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1210,6 +1210,15 @@ export const Showcases = [
     tags: ["governance"],
   },
   {
+    title: "Adastack.io",
+    description:
+      "Comprehensive Cardano ecosystem explorer: Explore intro guides, wallets, DApps, NFTs, games, governance, Project Catalyst, DAOs, development, sidechains, L2s and more.",
+    preview: require("./showcase/adastack.png"),
+    website: "https://www.adastack.io",
+    source: null,
+    tags: ["ecosystem", "educational"],
+  },
+  {
     title: "ADAM Cardano App",
     description:
       "Are you looking for an efficient way to connect and track your ADA wallets? ADAM Cardano app for iOS and Android is designed to help track all your Cardano stacking rewards, wallets and funds from a single, cohesive interface. ADAM app also exclusively provides stunning price widgets, packed with valuable price insights.",
