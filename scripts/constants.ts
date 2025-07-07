0c9a2251abbb99ed0a7f89b655e69e12c9469f13
0xBora <133051383+0xBora@users.noreply.github.com>
2025-07-07 22:36:39 +0200

    [Fix] Remove deprecated components and constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -8,17 +8,6 @@ const rl_raw_base_index_url: string =
 const rl_repo_raw_base_url: string =
   "https://raw.githubusercontent.com/Emurgo/cardano-serialization-lib/master/doc/getting-started/";
 
-// Token Registry constants
-const tr_docs_path: string = "./docs/native-tokens/token-registry";
-const tr_github_wiki: string =
-  "https://github.com/cardano-foundation/cardano-token-registry/wiki";
-const tr_url: string =
-  "https://github.com/cardano-foundation/cardano-token-registry/blob/master/";
-const tr_raw_wiki_url: string =
-  "https://raw.githubusercontent.com/wiki/cardano-foundation/cardano-token-registry/";
-const tr_overview_url: string =
-  "https://raw.githubusercontent.com/cardano-foundation/cardano-token-registry/master/README.md";
-
 // General constants
 const custom_edit_url = "\ncustom_edit_url: null"
 
@@ -28,10 +17,5 @@ export {
   rl_repo_base_url,
   rl_raw_base_index_url,
   rl_repo_raw_base_url,
-  tr_docs_path,
-  tr_github_wiki,
-  tr_url,
-  tr_raw_wiki_url,
-  tr_overview_url,
   custom_edit_url,
 };
