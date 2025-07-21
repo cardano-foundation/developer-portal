1659ba62795cc61583f70c6531f5f0a9fb088074
bora <133051383+0xBora@users.noreply.github.com>
2025-07-21 22:08:04 +0200

    [Ecosystem Outreach] - Showcase - Palmyra (#1571)
    * update: description
    
    * [Ecosystem Outreach] - Showcase - Palmyra

--- src/data/showcases.js
+++ src/data/showcases.js
@@ -1167,6 +1167,15 @@ export const Showcases = [
     source: "https://github.com/splashprotocol/splash-core",
     tags: ["dex", "opensource"],
   },
+  {
+    title: "Palmyra",
+    description:
+      "Empowering inclusion for underserved commodities through RWA Tokenization.",
+    preview: require("./showcase/palmyra.png"),
+    website: "https://palmeconomy.io/",
+    source: "https://github.com/zenGate-Global/winter-cardano",
+    tags: ["marketplace", "opensource"],
+  },
 ];
 
 export const TagList = Object.keys(Tags);
