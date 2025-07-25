efd0d96a3590c101bbe516f2f0e4d01592525317
0xBora <133051383+0xBora@users.noreply.github.com>
2025-07-26 00:41:31 +0200

    feature: add "Develop on Cardano" section to Builder Tools page

--- src/pages/portalhero.js
+++ src/pages/portalhero.js
@@ -13,18 +13,20 @@ function PortalHero({ title, description, cta, filename, url }) {
       <div className="container">
         <h1 className="hero__title">{title}</h1>
         <p className="hero__subtitle">{description}</p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx(
-              "button button--outline button--warn button--lg",
-              styles.getStarted
-            )}
-            {...(filename && {href: EDIT_URL, target: "_blank"})}
-            {...(!filename && {to: url})}
-          >
-            {cta}
-          </Link>
-        </div>
+        {cta && (
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                "button button--outline button--warn button--lg",
+                styles.getStarted
+              )}
+              {...(filename && {href: EDIT_URL, target: "_blank"})}
+              {...(!filename && {to: url})}
+            >
+              {cta}
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   );
