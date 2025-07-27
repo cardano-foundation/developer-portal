6e72499520aab6f2c100bd3cbdf166c1e422ca6c
0xBora <133051383+0xBora@users.noreply.github.com>
2025-07-27 02:30:33 +0200

    fix: update sidebar and constants paths for cardano-serialization-lib

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,6 @@
 // Rust Library constants
 const rl_static_resource_path: string = "/tree/master/doc/getting-started";
-const rl_docs_path: string = "./docs/get-started/cardano-serialization-lib";
+const rl_docs_path: string = "./docs/smart-contracts/cardano-serialization-lib";
 const rl_repo_base_url: string =
   "https://github.com/Emurgo/cardano-serialization-lib";
 const rl_raw_base_index_url: string =
