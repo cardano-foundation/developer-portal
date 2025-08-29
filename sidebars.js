6a0aa615801b115435e32ed21533260f54aa1967
0xBora <133051383+0xBora@users.noreply.github.com>
2025-08-29 17:41:42 +0200

    add pycardano

--- sidebars.js
+++ sidebars.js
@@ -43,6 +43,7 @@ module.exports = {
                   },
                 ],
               },
+              "get-started/pycardano",
               {
                 type: "category",
                 label: "cardano-cli",
